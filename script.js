259406a44f83d49cdaa6426768e074c62a5020fa
Andres Alama <alama33drech33@gmail.com>
2025-06-10 23:38:06 -0500

    Commit de actualización de la página

--- script.js
+++ script.js
@@ -1,70 +1,29 @@
-// Simulación de envío de formulario
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("¡Gracias por contactarnos! Te responderemos pronto.");
-    this.reset();
-});
+const header = document.querySelector(".header");
+const headerMenu = document.querySelector(".header__menu");
+const btnOpenClose = document.querySelector(".header__button");
+const links = document.querySelectorAll(".header__link");
 
-const btnOpen = document.getElementById('btn__open');
-const btnClose = document.getElementById('btn__close');
-const menu = document.getElementById('menu');
-const links = document.querySelectorAll('.menu__link');
+btnOpenClose.addEventListener("click", () => {
+    btnOpenClose.classList.toggle("header__button--active");
+    headerMenu.classList.toggle("header__menu--active");
+    document.body.classList.toggle("body--no-scroll");
+})
 
-
-function menuHamburger() {
-    menu.classList.toggle('menu--active');
-}
-
-btnOpen.addEventListener('click', menuHamburger)
-btnClose.addEventListener('click', menuHamburger)
-
+//Función que elimina la clase del menú para que pueda acceder a la sección seleccionada y desaparecer.
 links.forEach(link => {
     link.addEventListener('click', () => {
-        menu.classList.remove("menu--active");
+        headerMenu.classList.remove("header__menu--active");
+        btnOpenClose.classList.remove("header__button--active");
         document.body.classList.remove("body--no-scroll");
     })
 });
 
-const header = document.querySelector(".header");
-let lastScroll = 0;
+//Función que oculta el header
 
+let lastScroll = 0;
 window.onscroll = () => {
     const current = scrollY;
     header.style.transform = current > lastScroll ? "translateY(-100%)" : "translateY(0)";
     lastScroll = current;
-    header.style.backgroundColor = current > 0 ? "#0E65E5" : "transparent";
+    header.style.backgroundColor = current > 0 ? "var(--color-primario)" : "transparent";
 };
-
-function menuHamburger() {
-    menu.classList.toggle('menu--active');
-    document.body.classList.toggle('body--no-scroll');
-}
-
-// Funciones del modal
-
-const modal = document.getElementById("modal");
-const openBtn = document.getElementById("openModal");
-const closeBtn = document.getElementById("closeModal");
-
-// Mostrar modal automáticamente al cargar la página
-window.onload = () => {
-  modal.style.display = "block";
-};
-
-// Botón de apertura manual (por si quieres mantenerlo)
-openBtn.onclick = () => {
-  modal.style.display = "block";
-};
-
-closeBtn.onclick = () => {
-  modal.style.display = "none";
-};
-
-window.onclick = (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
-};
-
-
-
